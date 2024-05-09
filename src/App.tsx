225ea76479477a91ee50b8c7688bdf6e920b9d05
ameliapham <ptt.huong.8895@gmail.com>
2024-05-09 23:23:22 +0200

    Setup GlobalStyle

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,20 @@
+import { GlobalStyles } from "tss-react";
+
+
+
 export function App() {
-  return <h1>Hello World</h1>
+
+  return (
+    <>
+      <GlobalStyles styles={{
+        "html, body": {
+          "margin": 0,
+          "padding": 0,
+        }
+      }} />
+
+      <h1>Hello World</h1>
+
+    </>
+  )
 }
