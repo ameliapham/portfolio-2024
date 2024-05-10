d9e1100b757c7158fe8c7fa8e05efda0f4d63d87
ameliapham <ptt.huong.8895@gmail.com>
2024-05-10 19:37:36 +0200

    Setup ThemeProvider in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './App.tsx'
+import { ThemeProvider } from "./theme"
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+  <ThemeProvider>
     <App />
-  </React.StrictMode>,
+  </ThemeProvider>
 )
