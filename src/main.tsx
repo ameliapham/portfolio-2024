12d5a9635c626fc498f2b80dcf48d70f34a3f82a
ameliapham <ptt.huong.8895@gmail.com>
2024-05-14 18:49:28 +0200

    Update index for App file

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom/client'
-import { App } from './App/App.tsx'
+import { App } from './App'
 import { ThemeProvider } from "./theme"
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
