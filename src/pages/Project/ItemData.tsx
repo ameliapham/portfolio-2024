252b6718ea819facc8d363301c4a8d3f96ea970f
ameliapham <ptt.huong.8895@gmail.com>
2024-05-14 23:59:53 +0200

    Create DataItem

---
+++ src/pages/Project/ItemData.tsx
@@ -0,0 +1,36 @@
+import project1 from '../../assets/food-pho.webp';
+
+export type ItemData = {
+    img: string;
+    name: string;
+    des: string;
+    link?: string;
+}
+
+export const initialItems: ItemData[] = [
+    {
+        "img": project1,
+        "name": "Project 1",
+        "des": "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab, eum!",
+    }, {
+        "img": project1,
+        "name": "Project 1",
+        "des": "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab, eum!",
+    }, {
+        "img": project1,
+        "name": "Project 1",
+        "des": "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab, eum!",
+    }, {
+        "img": project1,
+        "name": "Project 1",
+        "des": "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab, eum!",
+    }, {
+        "img": project1,
+        "name": "Project 1",
+        "des": "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab, eum!",
+    }, {
+        "img": project1,
+        "name": "Project 1",
+        "des": "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab, eum!",
+    }
+];
