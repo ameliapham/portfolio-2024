2dd4178914d7f321148bdaa872aa5120d42920d8
ameliapham <ptt.huong.8895@gmail.com>
2024-05-16 04:54:04 +0200

    Create SeeMoreButton

---
+++ src/pages/Project/SeeMoreButton.tsx
@@ -0,0 +1,63 @@
+import { tss } from 'tss-react/mui'
+import Button from '@mui/material/Button';
+import { alpha } from "@mui/material/styles";
+
+
+type Props = {
+    className?: string;
+    onClick?: () => void;
+    children: React.ReactNode;
+};
+
+export function SeeMoreButton(props: Props) {
+
+    const { className, onClick, children } = props;
+    const { cx, classes } = useStyle()
+
+    return (
+        <div
+            className={cx(classes.root, className)}
+            onClick={onClick}
+        >
+            <Button className={classes.button}>
+                {children}
+            </Button>
+        </div>
+    )
+
+}
+
+const useStyle = tss
+    .create(({ theme }) => ({
+        "root": {
+            "justifyContent": "center",
+            "alignItems": "center",
+        },
+        "button": {
+            "position": "relative",
+            "color": theme.palette.text.primary,
+            "height": theme.spacing(6),
+            "padding": 0,
+            "overflow": "hidden",
+            "cursor": "pointer",
+
+            "&:hover": {
+                backgroundColor: "transparent",
+            },
+
+            "&::before": {
+                "content": "''",
+                "left": "0%",
+                "width": "0",
+                "height": "1px",
+                "backgroundColor": `${alpha(theme.palette.text.primary, 0.5)}`,
+                "transform": "translateX(-10px)",
+                "transition": "all 0.5s ease",
+            },
+
+            "&:hover::before": {
+                "width": "80px",
+            },
+        },
+
+    }))
