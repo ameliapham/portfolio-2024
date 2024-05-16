d8bc38a4d2c9fc713bc44f339c3afe574a56eee1
ameliapham <ptt.huong.8895@gmail.com>
2024-05-16 23:03:41 +0200

    Update Home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,6 +1,8 @@
 import { tss } from "tss-react/mui";
 import headVideo from "assets/headVideo.mp4";
 import headText from "assets/headText.png";
+import Typography from "@mui/material/Typography";
+import { SeeMoreButton } from "./SeeMoreButton";
 
 type Props = {
     className?: string;
@@ -8,22 +10,36 @@ type Props = {
 
 export function Home(props: Props) {
 
-        const { className } = props;
-        const { cx, classes } = useStyle();
+    const { className } = props;
+    const { cx, classes } = useStyle();
 
-        return (
-            <div className={cx(classes.root, className)}>
-                <video className={classes.video} autoPlay muted loop>
-                    <source src={headVideo} type="video/mp4" />
-                </video>
-                <img className={classes.img} src={headText} alt="headText" />
-            </div>
-        )
+    return (
+        <div className={cx(classes.root, className)}>
+            <video className={classes.video} autoPlay muted loop>
+                <source src={headVideo} type="video/mp4" />
+            </video>
+
+            <img className={classes.img} src={headText} alt="headText" />
+            
+            <Typography
+                className={classes.des}
+                variant="h6"
+            >
+                UX UI Designer / Front End Developer
+            </Typography>
+
+            <SeeMoreButton
+                className={classes.button}    
+            >
+                View My Work
+            </SeeMoreButton>
+        </div>
+    )
 }
 
 const useStyle = tss
     .withName({ Home })
-    .create(() => ({
+    .create(({ theme }) => ({
         "root": {
             "position": "relative",
             "display": "flex",
@@ -40,5 +56,19 @@ const useStyle = tss
             "position": "absolute",
             "width": "100%",
             "height": "100%",
-        }
+        },
+        "des": {
+            "position": "absolute",
+            "top": "62%",
+            "left": "50%",
+            "transform": "translate(-50%, -50%)",
+            "color": theme.palette.text.primary,
+            "textAlign": "center",
+        },
+        "button": {
+            "position": "absolute",
+            "top": "80%",
+            "left": "50%",
+            "transform": "translate(-50%, -50%)",
+        },
     }));
