44aa3821620782af9ccd8c4fdd131c6ad8a9070a
ameliapham <ptt.huong.8895@gmail.com>
2024-05-17 22:39:48 +0200

    Update typo and letter spacing of theme

--- src/theme/theme.tsx
+++ src/theme/theme.tsx
@@ -7,6 +7,22 @@ const typography = {
     },
     h2: {
         fontFamily: "PlayfairDisplay",
+        letterSpacing: "0.5px",
+    },
+    h3: {
+        fontFamily: "PlayfairDisplay",
+        letterSpacing: "0.5px",
+    },
+    h6: {
+        fontWeight: 300,
+        letterSpacing: "0.5px",
+    },
+    body1: {
+        fontWeight: 200,
+        letterSpacing: "0.5px",
+    },
+    button: {
+        letterSpacing: "0.5px",
     },
 };
 
@@ -16,7 +32,8 @@ export const theme = createTheme({
             default: "#0E0E10"
         },
         text: {
-            primary: "#8e97a3"
+            //primary: "#8e97a3"
+            primary: "#acb3b4"
         },
     },
     typography,
