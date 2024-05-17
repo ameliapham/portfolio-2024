adf4209ffa4b23b89e29aa242426af3bdf6e7782
ameliapham <ptt.huong.8895@gmail.com>
2024-05-17 15:19:15 +0200

    Setup tss-detecting unused classes

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,11 +8,14 @@ module.exports = {
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: [
+    'react-refresh',
+    'tss-unused-classes',
+  ],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+    '@typescript-eslint/no-explicit-any': 'off',
+    'tss-unused-classes/unused-classes': 'warn',
+    'react-refresh/only-export-components': 'off',
+    'react-hooks/exhaustive-deps': 'off'
   },
 }
