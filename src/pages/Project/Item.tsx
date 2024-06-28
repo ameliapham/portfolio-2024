e5a142ff4efb38bd6f06a4ae8ff2e94b76999bbd
ameliapham <ptt.huong.8895@gmail.com>
2024-06-28 22:15:18 +0200

    Update Item

--- src/pages/Project/Item.tsx
+++ src/pages/Project/Item.tsx
@@ -62,92 +62,102 @@ const animate = keyframes({
 const useStyles = tss
     .withName({ Item })
     .withNestedSelectors<"content">()
-    .create(({ classes, theme }) => ({
-        "root": {
-            "boxSizing": "border-box",
-            "width": "250px",
-            "height": theme.spacing(35),
-            "position": "absolute",
-            "top": "70%",
-            "transform": "translate(0, -50%)",
-            "borderRadius": "10px",
-            "backgroundPosition": "50% 50%",
-            "backgroundSize": "cover",
-            "transition": "0.5s ease-in-out",
-            "overflow": "hidden",
+    .create(({ classes, theme }) => {
+        const sideLength = "200px";
+        const left = "65%";
 
-            "&:nth-of-type(1), &:nth-of-type(2)": {
-                "top": 0,
-                "left": 0,
-                "transform": "translate(0, 0)",
-                "borderRadius": 0,
-                "width": "100%",
-                "height": "100%",
+        return {
+            "root": {
+                "boxSizing": "border-box",
+                "width": sideLength,
+                "height": `calc(${sideLength} * 1.2)`,
+                "position": "absolute",
+                "top": "75%",
+                "transform": "translate(0, -50%)",
+                "borderRadius": "10px",
+                "backgroundPosition": "50% 50%",
+                "backgroundSize": "cover",
                 "transition": "0.5s ease-in-out",
+                "overflow": "hidden",
 
-                "&::before": {
-                    "content": '""',
-                    "position": "absolute",
+                "&:nth-of-type(1), &:nth-of-type(2)": {
                     "top": 0,
                     "left": 0,
+                    "transform": "translate(0, 0)",
+                    "borderRadius": 0,
                     "width": "100%",
                     "height": "100%",
-                    "backgroundColor": "rgba(0, 0, 0, 0.5)",
-                    "zIndex": 1,
+                    "transition": "0.5s ease-in-out",
+
+                    "&::before": {
+                        "content": '""',
+                        "position": "absolute",
+                        "top": 0,
+                        "left": 0,
+                        "width": "100%",
+                        "height": "100%",
+                        "backgroundColor": "rgba(0, 0, 0, 0.5)",
+                        "zIndex": 1,
+                    }
+                },
+
+                "&:nth-of-type(3)": {
+                    "left": left,
+                    "zIndex": 2,
+                },
+
+                "&:nth-of-type(4)": {
+                    "left": `calc(${left} + 240px)`,
+                },
+
+                "&:nth-of-type(5)": {
+                    "left": `calc(${left} + 480px)`,
+                },
+
+                "&:nth-of-type(6)": {
+                    "left": `calc(${left} + 720px)`,
+                },
+
+                "&:nth-of-type(n + 7)": {
+                    "left": `calc(${left} + 960px)`,
+                    "opacity": 0,
+                },
+
+                [`&:nth-of-type(2) .${classes.content}`]: {
+                    "display": "block"
+                },
+
+            },
+            "content": {
+                "position": "absolute",
+                "top": "50%",
+                "left": theme.spacing(10),
+                "width": "30%",
+                "textAlign": "left",
+                "transform": "translate(0, -50%)",
+                "display": "none",
+                "color": theme.palette.text.primary,
+                "zIndex": 2,
+
+                "& button": {
+                    "opacity": 0,
+                    "animation": `${animate} 1s ease-in-out 0.6s 1 forwards`,
                 }
             },
-
-            "&:nth-of-type(3)": {
-                "left": "65%",
-                "zIndex": 2,
-            },
-
-            "&:nth-of-type(4)": {
-                "left": "calc(65% + 280px)",
-            },
-
-            "&:nth-of-type(5)": {
-                "left": "calc(65% + 560px)",
-            },
-
-            "&:nth-of-type(n + 6)": {
-                "left": "calc(65% + 840px)",
+            "name": {
                 "opacity": 0,
+                "animation": `${animate} 1s ease-in-out 1 forwards`,
             },
-
-            [`&:nth-of-type(2) .${classes.content}`]: {
-                "display": "block"
-            },
-
-        },
-        "content": {
-            "position": "absolute",
-            "top": "50%",
-            "left": theme.spacing(10),
-            "width": "30%",
-            "textAlign": "left",
-            "transform": "translate(0, -50%)",
-            "display": "none",
-            "color": theme.palette.text.primary,
-            "zIndex": 2,
-
-            "& button": {
+            "year": {
                 "opacity": 0,
-                "animation": `${animate} 1s ease-in-out 0.6s 1 forwards`,
+                "animation": `${animate} 1s ease-in-out 0.2s 1 forwards`,
+            },
+            "des": {
+                "marginTop": "20px",
+                "marginBottom": "20px",
+                "opacity": 0,
+                "animation": `${animate} 1s ease-in-out 0.3s 1 forwards`,
             }
-        },
-        "name": {
-            "opacity": 0,
-            "animation": `${animate} 1s ease-in-out 1 forwards`,
-        },
-        "year": {
-            "opacity": 0,
-            "animation": `${animate} 1s ease-in-out 0.2s 1 forwards`,
-        },
-        "des": {
-            "marginTop": "20px",
-            "marginBottom": "20px",
-            "opacity": 0,
-            "animation": `${animate} 1s ease-in-out 0.3s 1 forwards`,
         }
-    }));
+    });
+
