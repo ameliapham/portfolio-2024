f26f44afd5ba1ba53e97685e9d63eb49268c4362
ameliapham <ptt.huong.8895@gmail.com>
2024-09-13 23:22:44 +0200

    Update style of Item

--- src/pages/Project/Item.tsx
+++ src/pages/Project/Item.tsx
@@ -45,84 +45,90 @@ const useStyles = tss
     .withName({ Item })
     .withParams<{ position: number }>()
     .withNestedSelectors<"content">()
-    .create(({ classes, position }) => ({
-        root: {
-            "width": "200px",
-            "height": "300px",
-            "position": "absolute",
-            "top": "50%",
-            "transform": "translate(0, -50%)",
-            "borderRadius": "20px",
-            "boxShadow": "0 30px 50px #505050",
-            "backgroundPosition": "50% 50%",
-            "backgroundSize": "cover",
-            "display": "inline-block",
-            "transition": "0.4s",
-            ...(() => {
+    .create(({ classes, position, theme }) => {
+        const sideLength = "200px";
+        const left = "58%";
 
-                const oneAndTwo = {
-                    "top": 0,
-                    "left": 0,
-                    "transform": "translate(0, 0)",
-                    "borderRadius": 0,
-                    "width": "100%",
-                    "height": "100%",
-                } as const;
+return {
+    root: {
+        "width": sideLength,
+        "height": `calc(${sideLength} * 1.2)`,
+        "position": "absolute",
+        "top": "75%",
+        "transform": "translate(0, -50%)",
+        "borderRadius": "10px",
+        "backgroundPosition": "50% 50%",
+        "backgroundSize": "cover",
+        "display": "inline-block",
+        "transition": "0.5s",
+        ...(() => {
 
-                switch (position) {
-                    case 1: return oneAndTwo;
-                    case 2: return {
-                        ...oneAndTwo,
-                        [`& .${classes.content}`]: {
-                            "display": "block"
-                        }
-                    };
-                    case 3: return {
-                        "left": "50%",
-                    };
-                    case 4: return {
-                        "left": "calc(50% + 220px)",
-                    };
-                    case 5: return {
-                        "left": "calc(50% + 440px)",
-                    };
-                    default: return {
-                        "left": "calc(50% + 660px)",
-                        "opacity": 0,
-                    };
-                }
+            const oneAndTwo = {
+                "top": 0,
+                "left": 0,
+                "transform": "translate(0, 0)",
+                "borderRadius": 0,
+                "width": "100%",
+                "height": "100%",
+            } as const;
 
-            })(),
-        },
-        content: {
-            "position": "absolute",
-            "top": "50%",
-            "left": "100px",
-            "width": "300px",
-            "textAlign": "left",
-            "color": "#eee",
-            "transform": "translate(0, -50%)",
-            "display": "none",
-            "& button": {
-                "padding": "10px 20px",
-                "border": "none",
-                "cursor": "pointer",
-                "opacity": 0,
-                "animation": `${animate} 1s ease-in-out 0.6s 1 forwards`,
+            switch (position) {
+                case 1: return oneAndTwo;
+                case 2: return {
+                    ...oneAndTwo,
+                    [`& .${classes.content}`]: {
+                        "display": "block"
+                    }
+                };
+                case 3: return {
+                    "left": left,
+                };
+                case 4: return {
+                    "left": `calc(${left} + 240px)`,
+                };
+                case 5: return {
+                    "left": `calc(${left} + 480px)`,
+                };
+                case 6: return {
+                    "left": `calc(${left} + 720px)`,
+                };
+                default: return {
+                    "left": `calc(${left} + 960px)`,
+                    "opacity": 0,
+                };
             }
-        },
-        name: {
-            "fontSize": "40px",
-            "textTransform": "uppercase",
-            "fontWeight": "bold",
-            "opacity": 0,
-            "animation": `${animate} 1s ease-in-out 1 forwards`,
-        },
-        des: {
-            "marginTop": "10px",
-            "marginBottom": "20px",
-            "opacity": 0,
-            "animation": `${animate} 1s ease-in-out 0.3s 1 forwards`,
 
+        })(),
+    },
+    content: {
+        "position": "absolute",
+        "top": "50%",
+        "left": theme.spacing(10),
+        "width": "300px",
+        "textAlign": "left",
+        "color": "#eee",
+        "transform": "translate(0, -50%)",
+        "display": "none",
+        "& button": {
+            "padding": "10px 20px",
+            "border": "none",
+            "cursor": "pointer",
+            "opacity": 0,
+            "animation": `${animate} 1s ease-in-out 0.6s 1 forwards`,
         }
-    }));
+    },
+    name: {
+        "fontSize": "40px",
+        "textTransform": "uppercase",
+        "fontWeight": "bold",
+        "opacity": 0,
+        "animation": `${animate} 1s ease-in-out 1 forwards`,
+    },
+    des: {
+        "marginTop": "10px",
+        "marginBottom": "20px",
+        "opacity": 0,
+        "animation": `${animate} 1s ease-in-out 0.3s 1 forwards`,
+
+    }
+}})
