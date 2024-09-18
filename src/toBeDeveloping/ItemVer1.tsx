14d932b23e2983946b5cdb226cac4d05a6723f20
ameliapham <ptt.huong.8895@gmail.com>
2024-09-18 20:45:02 +0200

    Move unfixing project in waiting

---
+++ src/toBeDeveloping/ItemVer1.tsx
@@ -0,0 +1,169 @@
+import { keyframes } from "tss-react";
+import { tss } from "tss-react/mui";
+import { type ItemData } from "data/projectData";
+import Typography from "@mui/material/Typography";
+import { SeeMoreButton } from "shared/SeeMoreButton";
+
+
+type ItemProps = {
+    className?: string;
+    itemData: ItemData;
+    onClick: (src : string) => void;
+}
+
+export function Item(props: ItemProps) {
+    const { className, itemData, onClick } = props;
+    const { cx, classes } = useStyles();
+
+    return (
+        <div
+            className={cx(classes.root, className)}
+            style={({ backgroundImage: `url(${itemData.img})` })}
+        >
+            <div className={classes.content}>
+                <Typography
+                    variant="body1"
+                    className={classes.year}
+                >
+                    {itemData.year}
+                </Typography>
+                <Typography
+                    variant="h2"
+                    className={classes.name}
+                >
+                    {itemData.name}
+                </Typography>
+                <Typography
+                    variant="body1"
+                    className={classes.des}
+                >
+                    {itemData.des}
+                </Typography>
+                <SeeMoreButton onClick={() => {}}>
+                    See More
+                </SeeMoreButton>
+            </div>
+        </div >
+    );
+}
+
+const animate = keyframes({
+    "from": {
+        "opacity": 0,
+        "transform": "translate(0, 100px)",
+        "filter": "blur(200px)"
+    },
+    "to": {
+        "opacity": 1,
+        "transform": "translate(0)",
+        "filter": "blur(0)"
+    }
+});
+
+const useStyles = tss
+    .withName({ Item })
+    .withNestedSelectors<"content">()
+    .create(({ classes, theme }) => {
+        const sideLength = "200px";
+        const left = "65%";
+
+        return {
+            "root": {
+                "boxSizing": "border-box",
+                "width": sideLength,
+                "height": `calc(${sideLength} * 1.2)`,
+                "position": "absolute",
+                "top": "75%",
+                "transform": "translate(0, -50%)",
+                "borderRadius": "10px",
+                "backgroundPosition": "50% 50%",
+                "backgroundSize": "cover",
+                "transition": "0.5s ",
+                "overflow": "hidden",
+
+                "&:nth-of-type(1), &:nth-of-type(2)": {
+                    "top": 0,
+                    "left": 0,
+                    "transform": "translate(0, 0)",
+                    "borderRadius": 0,
+                    "width": "100%",
+                    "height": "100%",
+
+                    
+                    "&::before": {
+                        "content": '""',
+                        "position": "absolute",
+                        "top": 0,
+                        "left": 0,
+                        "width": "100%",
+                        "height": "100%",
+                        "backdropFilter": "brightness(91%)",
+                        "zIndex": 1,
+                    }
+                        
+                },
+
+                "&:nth-of-type(3)": {
+                    "left": left,
+                    "zIndex": 2,
+                },
+
+                "&:nth-of-type(4)": {
+                    "left": `calc(${left} + 240px)`,
+                },
+
+                "&:nth-of-type(5)": {
+                    "left": `calc(${left} + 480px)`,
+                },
+
+                "&:nth-of-type(6)": {
+                    "left": `calc(${left} + 720px)`,
+                },
+
+                "&:nth-of-type(n + 7)": {
+                    "left": `calc(${left} + 960px)`,
+                    "opacity": 0,
+                },
+
+                [`&:nth-of-type(2) .${classes.content}`]: {
+                    "display": "block"
+                },
+
+            },
+            "content": {
+                "position": "absolute",
+                "top": "50%",
+                "left": theme.spacing(10),
+                "width": "30%",
+                "textAlign": "left",
+                "transform": "translate(0, -50%)",
+                "display": "none",
+                "color": theme.palette.text.primary,
+                "zIndex": 2,
+                "padding": theme.spacing(5),
+                "borderRadius": "10px",
+                "backdropFilter": "brightness(70%)",
+
+
+                "& button": {
+                    "opacity": 0,
+                    "animation": `${animate} 1s ease-in-out 0.6s 1 forwards`,
+                }
+            },
+            "name": {
+                "opacity": 0,
+                "animation": `${animate} 1s ease-in-out 1 forwards`,
+            },
+            "year": {
+                "opacity": 0,
+                "animation": `${animate} 1s ease-in-out 0.2s 1 forwards`,
+            },
+            "des": {
+                "marginTop": "20px",
+                "marginBottom": "20px",
+                "opacity": 0,
+                "animation": `${animate} 1s ease-in-out 0.3s 1 forwards`,
+            }
+        }
+    });
+
---
+++ src/toBeDeveloping/ProjectVer1.tsx
@@ -0,0 +1,152 @@
+import { tss } from "tss-react/mui";
+import { useEffect, useState } from "react";
+import { Item } from "./ItemVer1";
+import { projectData } from "data/projectData";
+import { useConstCallback } from "powerhooks/useConstCallback";
+import { assert } from "tsafe/assert";
+import { useScrollWidth } from "hooks/useScrollHeight";
+//import LinearProgress from '@mui/material/LinearProgress';
+import { useDomRect } from "powerhooks/useDomRect";
+import { useMergeRefs } from "powerhooks/useMergeRefs";
+import { Zen } from "../pages/Project/pages/zen/Zen";
+
+
+type Props = {
+    pages: "zen" | "dame" | "gmeta" | "iso" | "arti" | "gili" | "famed" | "badgeur";
+};
+
+
+export function Project(props: Props) {
+
+    const { pages } = props;
+
+    const [sliderElement, setSliderElement] = useState<HTMLElement | null>(null);
+    const [refIsAnimating] = useState({ "current": false });
+
+    const { ref: scrollableContentRef, domRect: { width: scrollableContentWidth } } = useDomRect();
+
+    const ref = useMergeRefs([scrollableContentRef, setSliderElement]);
+
+    const { classes, css } = useStyles();
+    const { scrollWidth } = useScrollWidth();
+
+    //const [isAnimating, setIsAnimating]= useState(false);
+
+    const handleNext = useConstCallback(() => {
+        if (refIsAnimating.current) {
+            return;
+        }
+
+        refIsAnimating.current = true;
+
+        assert(sliderElement !== null);
+
+        const firstChild = sliderElement.children[0];
+        sliderElement.appendChild(firstChild);
+
+        setTimeout(() => {
+            refIsAnimating.current = false;
+        }, 500);
+    });
+
+    const handlePrev = useConstCallback(() => {
+        if (refIsAnimating.current) {
+            return;
+        }
+
+        refIsAnimating.current = true;
+
+        assert(sliderElement !== null);
+
+        const lastChild = sliderElement.children[sliderElement.children.length - 1];
+        sliderElement.prepend(lastChild);
+
+        setTimeout(() => {
+            refIsAnimating.current = false;
+        }, 500);
+    });
+
+    useEffect(() => {
+        const onWheel = (e: WheelEvent) => {
+            if (e.deltaY < 0) {
+                handlePrev();
+            } else {
+                handleNext();
+            }
+        };
+
+        window.addEventListener('wheel', onWheel);
+
+        return () => {
+            window.removeEventListener('wheel', onWheel);
+        };
+    }, []);
+
+    console.log('Scroll Height:', scrollWidth);
+    console.log('Scrollable Content Height:', scrollableContentWidth);
+
+    const pageData = (() => {
+        switch (pages) {
+            case "zen":
+                return <Zen />;
+        }
+    })();
+
+    return (
+        <div className={classes.container} >
+            <div ref={ref} >
+                {projectData.map(itemData => (
+                    <Item
+                        className={css({ border: "3px solid red !important" })}
+                        key={itemData.name}
+                        itemData={itemData}
+                        onClick={() => {
+                            //console.log('Clicked:', itemData.page);
+                        }}
+                    />
+                ))}
+            </div>
+            {/*
+                <LinearProgress
+                    classes={{
+                        "bar": classes.progressBar
+                    }}
+                    className={classes.progress}
+                    variant="determinate"
+                    value={(scrollWidth / (scrollableContentWidth - window.innerHeight)) * 100}
+                />
+            */}
+        </div>
+    );
+}
+
+const useStyles = tss
+    .withName({ Project })
+    .create(({ theme }) => ({
+        "container": {
+            "boxSizing": "border-box",
+            "position": "absolute",
+            "top": "50%",
+            "left": "50%",
+            "transform": "translate(-50%, -50%)",
+            "width": "100%",
+            "height": "100%",
+            "background": theme.palette.background.default,
+            "overflow": "hidden",
+            "transition": "background-image 0.5s",
+        },
+        /*
+        "progress": {
+            "position": "absolute",
+            "width": "50%",
+            "bottom": "30px",
+            "left": "50%",
+            "transform": "translate(-50%, 0)",
+            "zIndex": 2,
+            "backgroundColor": "black",
+        },
+        "progressBar": {
+            "backgroundColor": "white"
+        },
+        */
+    }));
