534a36972856508306e6539ef13399e3ca0ee730
ameliapham <ptt.huong.8895@gmail.com>
2024-09-26 14:23:50 +0200

    Update style of SeeMoreButton

--- src/shared/SeeMoreButton.tsx
+++ src/shared/SeeMoreButton.tsx
@@ -12,12 +12,18 @@ export function SeeMoreButton(props: Props) {
     const { className, onClick, children } = props;
     const { cx, classes } = useStyles();
 
+    
+
     return (
         <div className={cx(classes.root, className)} onClick={onClick}>
-            <Button className={classes.button}>{children}</Button>
+            <Button className={classes.button}>
+                <span className={classes.buttonText}>{children}</span>
+                </Button>
         </div>
     );
 }
+
+
 
 const useStyles = tss.create(({ theme }) => ({
     root: {
@@ -29,25 +35,35 @@ const useStyles = tss.create(({ theme }) => ({
         color: theme.palette.text.primary,
         height: theme.spacing(6),
         padding: 0,
-        overflow: "hidden",
         cursor: "pointer",
 
         "&:hover": {
-            backgroundColor: "transparent"
+            backgroundColor: "transparent",
         },
 
         "&::before": {
             content: "''",
             left: 0,
-            width: 0,
+            width:  theme.spacing(8),
             height: theme.spacing(0.15),
             backgroundColor: `${alpha(theme.palette.text.primary, 0.5)}`,
-            transform: "translateX(-10px)",
-            transition: "all 0.5s ease"
+            transition: "all 0.5s ease",
         },
 
         "&:hover::before": {
+            right: "0",
+            transform: "translateX(50px)",
             width: theme.spacing(3)
-        }
+        },
+
+        "&:hover span": {
+            transform: "translateX(60px)",
+            transition: "all 0.5s ease"
+        },
+    },
+    buttonText: {
+        transition: "transform 0.5s ease",
+        transform: "translateX(10px)"
     }
+
 }));
