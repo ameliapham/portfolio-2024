48be65ba3f0d19cf95a497ea0f58f84f5bb759e0
ameliapham <ptt.huong.8895@gmail.com>
2024-10-15 21:47:09 +0200

    Create useScrollNavigation

---
+++ src/hooks/useScrollNavigation.ts
@@ -0,0 +1,37 @@
+import { Evt } from "evt";
+import { throttleTime } from "evt/operators";
+import { useEvt } from "evt/hooks";
+import { useConstCallback } from "powerhooks/useConstCallback";
+
+
+const evtScrollNavigation = Evt.create<"up" | "down">();
+
+let lastDeltaYAbs = 0;
+
+window.addEventListener('wheel', event => {
+
+    const deltaYAbs = Math.abs(event.deltaY);
+
+    if (deltaYAbs - lastDeltaYAbs > 2) {
+        evtScrollNavigation.post(event.deltaY > 0 ? "down" : "up");
+    }
+
+    lastDeltaYAbs = deltaYAbs;
+
+});
+
+const evtScrollNavigationThrottled = evtScrollNavigation.pipe(throttleTime(500));
+
+export function useScrollNavigation(onScrollNavigation: (direction: "up" | "down") => void) {
+
+    const onScrollNavigation_stable = useConstCallback(onScrollNavigation);
+
+    useEvt(ctx => {
+
+        evtScrollNavigationThrottled.attach(ctx, direction => {
+            onScrollNavigation_stable(direction);
+        });
+
+    }, []);
+
+}
