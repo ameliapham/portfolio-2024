c4bdcc8fb7571a50e41c4e8a3800841ba67fa61a
ameliapham <ptt.huong.8895@gmail.com>
2024-10-23 17:42:13 +0200

    Create Page4 of Zen

---
+++ src/pages/Project/ProjectDetails/Zen/Page4.tsx
@@ -0,0 +1,115 @@
+import { tss } from "tss-react/mui";
+import { keyframes } from "tss-react";
+import zenIpad from "assets/zen-ipad.png";
+import Typography from "@mui/material/Typography";
+import { alpha } from "@mui/material/styles";
+
+type Props = {
+    className?: string;
+};
+
+export function Page4(props: Props) {
+    const { className } = props;
+    const { cx, classes } = useStyles();
+
+    return (
+        <div className={cx(classes.root, className)}>
+            <Typography variant="body1" className={classes.infoGauche}>
+                Responsive with
+                <ul>
+                    <li>Phone</li>
+                    <li>Tablet</li>
+                    <li>Computer screen</li>
+                </ul>
+            </Typography>
+            <img src={zenIpad} alt="Zen Gourmet website on a phone" className={classes.image} />
+            <div className={classes.infoDroit}>
+                <Typography variant="body1">
+                    Multiple languages available
+                    <ul>
+                        <li>English</li>
+                        <li>French</li>
+                        <li>German (incoming)</li>
+                    </ul>
+                </Typography>
+            </div>
+        </div>
+    );
+}
+
+const animate = keyframes({
+    from: {
+        opacity: 0,
+        transform: "translate(0, 100px)",
+        filter: "blur(30px)"
+    },
+    to: {
+        opacity: 1,
+        transform: "translate(0)",
+        filter: "blur(0)"
+    }
+});
+
+const useStyles = tss
+    .withName({ name: "ZenPage4" })
+    .create(({ theme }) => {
+        return {
+            root: {
+                color: theme.palette.text.primary,
+                display: "grid",
+                gridTemplateColumns: "1fr 1fr 1fr 1fr",
+                gridTemplateRows: "1fr 1fr 1fr",
+                alignItems: "center"
+            },
+            image: {
+                gridColumn: "1/5",
+                gridRow: "1/4",
+                width: "100%",
+                height: "100%",
+                maxHeight: "700px",
+                objectFit: "contain",
+                zIndex: 1,
+                opacity: 0,
+                animation: `${animate} 0.5s ease-in-out 0.2s 1 forwards`
+            },
+            infoGauche: {
+                gridColumn: "1/3",
+                gridRow: "1",
+                position: "relative",
+                opacity: 0,
+                animation: `${animate} 0.5s ease-in-out 0.4s 1 forwards`,
+
+                "&::after": {
+                    content: "''",
+                    position: "absolute",
+                    bottom: "0%",
+                    left: "0%",
+                    width: "100%",
+                    height: theme.spacing(0.1),
+                    backgroundColor: `${alpha(theme.palette.text.primary, 0.2)}`,
+                    transition: "all 0.5s ease"
+                },
+            },
+            infoDroit: {
+                gridColumn: "3/5",
+                gridRow: "3",
+                opacity: 0,
+                animation: `${animate} 0.5s ease-in-out 0.4s 1 forwards`,
+                display: "flex",
+                justifyContent: "end",
+                position: "relative",
+
+                "&::after": {
+                    content: "''",
+                    position: "absolute",
+                    bottom: "0%",
+                    right: "0%",
+                    width: "100%",
+                    height: theme.spacing(0.1),
+                    backgroundColor: `${alpha(theme.palette.text.primary, 0.2)}`,
+                    transition: "all 0.5s ease"
+                },
+            },
+
+        };
+    });
--- src/pages/Project/ProjectDetails/Zen/Zen.tsx
+++ src/pages/Project/ProjectDetails/Zen/Zen.tsx
@@ -2,6 +2,7 @@ import { tss } from "tss-react/mui";
 import { Page1 } from "./Page1";
 import { Page2 } from "./Page2";
 import { Page3 } from "./Page3";
+import { Page4 } from "./Page4";
 
 type Props = {
     className?: string;
@@ -15,13 +16,15 @@ export function Zen(props: Props) {
         <>
             <div className={cx(classes.root, className)}>
                 {(() => {
-                    switch (detailsIndex % 3) {
+                    switch (detailsIndex % 4) {
                         case 0:
                             return <Page1 />;
                         case 1:
                             return <Page2 />;
                         case 2:
                             return <Page3 />;
+                        case 3:
+                            return <Page4 />;
                     }
                 })()}
             </div>
