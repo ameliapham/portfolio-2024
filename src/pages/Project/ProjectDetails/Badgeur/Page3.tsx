af36eab1ce5851c043ac3b5ea8225d9237226b89
ameliapham <ptt.huong.8895@gmail.com>
2024-10-26 01:24:17 +0200

    Update content & images of Page 3 Badgeur

--- src/pages/Project/ProjectDetails/Badgeur/Page3.tsx
+++ src/pages/Project/ProjectDetails/Badgeur/Page3.tsx
@@ -1,6 +1,11 @@
 import { tss } from "tss-react/mui";
 import { keyframes } from "tss-react";
-import badgeurPhone from "assets/badgeur-phone.png";
+import badgeurPhone from "assets/badgeur-phone.svg";
+import badgeurLogo from "assets/badgeur-logo.svg";
+import badgeurColor1 from "assets/badgeur-color1.svg";
+import badgeurColor2 from "assets/badgeur-color2.svg";
+import badgeurColor3 from "assets/badgeur-color3.svg";
+
 
 type Props = {
     className?: string;
@@ -12,7 +17,11 @@ export function Page3(props: Props) {
 
     return (
         <div className={cx(classes.root, className)}>
-            <img src={badgeurPhone} alt="Badgeur phone" className={classes.image} />
+            <img src={badgeurPhone} alt="Badgeur phone" className={cx(classes.image, classes.phone)} />
+            <img src={badgeurLogo} alt="Badgeur logo" className={cx(classes.image, classes.logo)} />
+            <img src={badgeurColor1} alt="Badgeur color 1" className={cx(classes.image, classes.color1)} />
+            <img src={badgeurColor2} alt="Badgeur color 2" className={cx(classes.image, classes.color2)} />
+            <img src={badgeurColor3} alt="Badgeur color 3" className={cx(classes.image, classes.color3)} />
         </div>
     );
 }
@@ -30,26 +39,39 @@ const animate = keyframes({
     }
 });
 
-const useStyles = tss.withName({ name: "BadgeurPage3" }).create(({theme}) => {
+const useStyles = tss.withName({ name: "BadgeurPage3" }).create(() => {
     return {
         root: {
-            color: theme.palette.text.primary,
             display: "grid",
-            gridTemplateColumns: "1fr 3fr 1fr 1fr",
-            gridTemplateRows: "1fr 1fr 1fr",
-            alignItems: "center",
-            border: "1px solid red"
+            gridTemplateColumns: "1fr 1fr"
         },
         image: {
-            gridColumn: "1 / 4",
-            gridRow: "1 / 4",
+            gridColumn: "1/3",
+            gridRow: "1/3",
             width: "100%",
             height: "100%",
-            maxHeight: "700px",
-            objectFit: "cover",
+            maxHeight: "600px",
+            objectFit: "contain"
+        },
+        phone: {
             opacity: 0,
-            animation: `${animate} 0.5s ease-in-out 0.2s 1 forwards`,
-            border: "1px solid blue"
+            animation: `${animate} 0.5s ease-in-out 0.2s 1 forwards`
+        },
+        logo: {
+            opacity: 0,
+            animation: `${animate} 0.5s ease-in-out 0.4s 1 forwards`
+        },
+        color1: {
+            opacity: 0,
+            animation: `${animate} 0.4s ease-in-out 0.5s 1 forwards`
+        },
+        color2: {
+            opacity: 0,
+            animation: `${animate} 0.4s ease-in-out 0.6s 1 forwards`
+        },
+        color3: {
+            opacity: 0,
+            animation: `${animate} 0.4s ease-in-out 0.7s 1 forwards`
         }
     };
 });
