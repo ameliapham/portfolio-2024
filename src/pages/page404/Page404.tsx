1fca0aec93f852a2b6434293ead8a7b2b5e92668
ameliapham <ptt.huong.8895@gmail.com>
2024-10-27 14:10:51 +0100

    Create Page404

---
+++ src/pages/page404/Page404.tsx
@@ -0,0 +1,31 @@
+
+import { tss } from "tss-react";
+
+export type Props = {
+    className?: string;
+};
+
+export default function Page(props: Props) {
+
+    const { className } = props;
+
+    const { cx, classes } = useStyles();
+
+    return (
+        <div className={cx(classes.root, className)}>
+            <div>
+                <h1>Not Found 🥲</h1>
+            </div>
+        </div>
+    );
+
+}
+
+const useStyles = tss.withName({ Page }).create({
+    root: {
+        border: '5px solid yellow',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    }
+});
---
+++ src/pages/page404/route.ts
@@ -0,0 +1,13 @@
+import {
+    createRouter,
+    defineRoute,
+    createGroup,
+    type Route
+} from "type-route";
+export const routeDefs = {
+    page404: defineRoute("/404")
+};
+
+export const routeGroup = createGroup(Object.values(createRouter(routeDefs).routes));
+
+export type PageRoute = Route<typeof routeGroup>;
