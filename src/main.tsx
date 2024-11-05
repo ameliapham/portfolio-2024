bbe497b8387a61458ec8c75a71f6dfc08915be46
ameliapham <ptt.huong.8895@gmail.com>
2024-11-05 04:40:45 +0100

    Update main

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,6 @@
 import ReactDOM from "react-dom/client";
 import { App } from "./App";
-import { PageIdProvider } from "hooks/usePageId";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-    <PageIdProvider defaultPageId="home">
-        <App />
-    </PageIdProvider>
+    <App />
 );
