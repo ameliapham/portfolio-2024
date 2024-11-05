1cbffe36b4d3d467e7836b68a2439d3c33258b4f
ameliapham <ptt.huong.8895@gmail.com>
2024-11-05 04:37:09 +0100

    Create tss.ts

---
+++ src/tss.ts
@@ -0,0 +1,22 @@
+import { createTss } from "tss-react";
+import { useTheme } from "@mui/material/styles";
+import { useWindowInnerSize } from "powerhooks/useWindowInnerSize";
+export { GlobalStyles, keyframes } from "tss-react";
+
+function useContext() {
+    const theme = useTheme();
+
+    const headerHeight= theme.spacing(10);
+
+    const { windowInnerWidth } = useWindowInnerSize();
+
+    return {
+        theme,
+        headerHeight,
+        windowInnerWidth
+    };
+}
+
+export const { tss } = createTss({
+    useContext
+});
