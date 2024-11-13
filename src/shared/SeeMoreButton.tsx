d1d1cd18d322e85ea8502bffe32c3a4c0dd29357
ameliapham <ptt.huong.8895@gmail.com>
2024-11-13 18:45:29 +0100

    Upgrade SeeMoreButton style

--- src/shared/SeeMoreButton.tsx
+++ src/shared/SeeMoreButton.tsx
@@ -4,66 +4,84 @@ import { alpha } from "@mui/material/styles";
 
 type Props = {
     className?: string;
+    href?: string;
     onClick?: () => void;
-    children: React.ReactNode;
     disabled?: boolean;
+    hyphenPosition?: "left" | "right";
+    children: React.ReactNode;
 };
 
 export function SeeMoreButton(props: Props) {
-    const { className, onClick, children, disabled } = props;
-    const { cx, classes } = useStyles();
+    const { className, onClick, children, disabled, href, hyphenPosition } = props;
+    const { cx, classes } = useStyles({
+        isBefore: hyphenPosition === "right"
+    });
 
     return (
         <Button
-            className={cx(classes.button, className)}
+            className={cx(classes.root, className)}
+            href={href}
             onClick={onClick}
             disabled={disabled}
         >
-            <span>{children}</span>
+            <span className={classes.buttonText}>{children}</span>
         </Button>
     );
 }
 
-const useStyles = tss.create(({ theme }) => ({
-    button: {
-        position: "relative",
-        color: theme.palette.text.primary,
-        height: theme.spacing(6),
-        padding: 0,
-        cursor: "pointer",
-        width: "200px",
-        left: "0",
-
-        "&:hover": {
-            backgroundColor: "transparent"
-        },
-
-        "&::before": {
-            content: "''",
-            position: "absolute",
+const useStyles = tss
+    .withName({ SeeMoreButton })
+    .withNestedSelectors<"buttonText">()
+    .withParams<{
+        isBefore: boolean;
+    }>()
+    .create(({ theme, isBefore, classes }) => ({
+        root: {
+            position: "relative",
+            color: theme.palette.text.primary,
+            height: theme.spacing(6),
+            padding: 0,
+            cursor: "pointer",
+            width: "200px",
             left: "0",
-            width: theme.spacing(8),
-            height: theme.spacing(0.15),
-            backgroundColor: `${alpha(theme.palette.text.primary, 0.5)}`,
-            transition: "all 0.4s ease"
-        },
 
-        "&:hover::before": {
-            right: "0",
-            transform: "translateX(50px)",
-            width: theme.spacing(3)
-        },
+            "&:hover": {
+                backgroundColor: "transparent"
+            },
 
-        span: {
+            "&::before": isBefore && {
+                content: "''",
+                position: "absolute",
+                left: "0",
+                width: theme.spacing(8),
+                height: theme.spacing(0.15),
+                backgroundColor: alpha(theme.palette.text.primary, 0.5),
+                transition: "all 0.4s ease",
+            },
+            "&::after": !isBefore && {
+                content: "''",
+                position: "absolute",
+                right: "0",
+                width: theme.spacing(8),
+                height: theme.spacing(0.15),
+                backgroundColor: alpha(theme.palette.text.primary, 0.5),
+                transition: "all 0.4s ease",
+            },
+
+            [`&:hover::${isBefore ? "before" : "after"}`]: {
+                transform: `translateX(${isBefore ? "" : "-"}50px)`,
+                width: theme.spacing(3)
+            },
+
+            [`&:hover .${classes.buttonText}`]: {
+                transform: `translateX(${isBefore ? "" : "-"}20px)`,
+                transition: "all 0.4s ease"
+            }
+        },
+        buttonText: {
             position: "absolute",
             left: theme.spacing(8),
             transition: "all 0.4s ease",
             transform: "translateX(10px)"
-        },
-
-        "&:hover span": {
-            transform: "translateX(20px)",
-            transition: "all 0.4s ease"
         }
-    }
-}));
+    }));
