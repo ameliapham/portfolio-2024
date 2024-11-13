cad54dabeaa1a17cc1af8df084a20f1dfc2891f4
ameliapham <ptt.huong.8895@gmail.com>
2024-11-13 18:49:29 +0100

    Add NavComponent in page Zen

--- src/pages/projects/ProjectDetails/Zen/Zen.tsx
+++ src/pages/projects/ProjectDetails/Zen/Zen.tsx
@@ -16,23 +16,49 @@ type Props = {
 export function Zen(props: Props) {
     const { className, route } = props;
     const { cx, classes } = useStyles();
+
     return (
         <>
             <div className={cx(classes.root, className)}>
-                {(() => {
-                    switch (route.params.detailsIndex) {
-                        case 0:
-                            return <Page1 />;
-                        case 1:
-                            return <Page2 />;
-                        case 2:
-                            return <Page3 />;
-                        case 3:
-                            return <Page4 />;
-                        default:
-                            assert(false);
+                <div className={classes.content}>
+                    {(() => {
+                        switch (route.params.detailsIndex) {
+                            case 0:
+                                return <Page1 />;
+                            case 1:
+                                return <Page2 />;
+                            case 2:
+                                return <Page3 />;
+                            case 3:
+                                return <Page4 />;
+                            default:
+                                assert(false);
+                        }
+                    })()}
+                </div>
+                <NavComponent
+                    className={classes.navComponent}
+                    previousLink={
+                        route.params.detailsIndex === 0
+                            ? undefined
+                            : routes[route.name]({
+                                ...route.params,
+                                detailsIndex: route.params.detailsIndex - 1
+                            }).link
                     }
-                })()}
+                    nextLink={
+                        route.params.detailsIndex === 3
+                            ? undefined
+                            : routes[route.name]({
+                                ...route.params,
+                                detailsIndex: route.params.detailsIndex + 1
+                            }).link
+                    }
+                    processPercentage={
+                        (route.params.detailsIndex / 3) * 100
+                        //(route.params.detailsIndex + 1) / 4 * 100
+                    }
+                />
             </div>
         </>
     );
@@ -41,8 +67,17 @@ export function Zen(props: Props) {
 const useStyles = tss.withName({ Zen }).create(() => {
     return {
         root: {
+            border: "1px solid red",
             height: "100%",
-            width: "100%"
+            width: "100%",
+            display: "flex",
+            flexDirection: "column",
+        },
+        content: {
+            flex: 2,
+            padding: "0 10%"
+        },
+        navComponent: {
         }
     };
 });
