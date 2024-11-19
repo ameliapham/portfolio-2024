46095354ccfb3d75b0dcb3c9cfb359e00f6b185f
Joseph Garrone <joseph.garrone.gj@gmail.com>
2024-11-19 22:50:29 +0100

    Implement fixed background on scrollable page

--- src/pages/projects/ProjectGallery/ProjectGalleryMobile.tsx
+++ src/pages/projects/ProjectGallery/ProjectGalleryMobile.tsx
@@ -1,10 +1,24 @@
 import type { Props } from "./Props";
+import { useStyles } from "tss";
+import pZenWebpUrl from "assets/p-zen.webp";
 
 export default function ProjectGalleryMobile(props: Props) {
     const { className } = props;
 
+    const { cx, css } = useStyles();
+
     return (
-        <div className={className}>
+        <div
+            className={cx(
+                css({
+                    backgroundImage: `url('${pZenWebpUrl}')` /* Replace with your image URL */,
+                    backgroundSize: "cover" /* Scales the background image to cover the div */,
+                    backgroundPosition: "center" /* Center the background image */,
+                    backgroundAttachment: "fixed" /* Fix the background image */
+                }),
+                className
+            )}
+        >
             {new Array(300).fill("").map((_, index) => (
                 <p key={index}>
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor, nunc nec{" "}
