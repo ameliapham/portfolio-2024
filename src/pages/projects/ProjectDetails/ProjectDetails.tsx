e1dfcac82727090cceab24df026f952adcd5621c
ameliapham <ptt.huong.8895@gmail.com>
2024-11-22 23:58:08 +0100

    Update details project paddingTop

--- src/pages/projects/ProjectDetails/ProjectDetails.tsx
+++ src/pages/projects/ProjectDetails/ProjectDetails.tsx
@@ -60,14 +60,14 @@ export function ProjectDetails(props: Props) {
 const useStyles = tss
     .withName({ ProjectDetails })
     .withParams<{ projectId: Project["id"] }>()
-    .create(({ theme, projectId }) => {
+    .create(({ theme, projectId, headerHeight }) => {
         const project = projects.find(project => project.id === projectId);
         const backgroundImage = project ? `url(${project.backgroundUrl})` : "none";
         //assert(project !== undefined);
 
         return {
             root: {
-                padding: `${theme.spacing(2)} ${theme.spacing(10)} ${theme.spacing(4)} ${theme.spacing(10)}`,
+                padding: `${headerHeight} ${theme.spacing(10)} ${theme.spacing(4)} ${theme.spacing(10)}`,
                 position: "relative",
                 display: "flex",
                 flexDirection: "column",
