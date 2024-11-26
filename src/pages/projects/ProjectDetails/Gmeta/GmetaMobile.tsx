194e19ede25d29ddb79827fa025ea390da98798e
ameliapham <ptt.huong.8895@gmail.com>
2024-11-26 03:24:53 +0100

    Update Gmeta project with mobile version

---
+++ src/pages/projects/ProjectDetails/Gmeta/GmetaMobile.tsx
@@ -0,0 +1,29 @@
+import { tss } from "tss-react/mui";
+import { Page1 } from "./Page1";
+import { Page2 } from "./Page2";
+import { Props } from "../../Props"
+
+export default function Gmeta(props: Props) {
+    const { className } = props;
+    const { cx, classes } = useStyles();
+    return (
+        <>
+            <div className={cx(classes.root, className)}>
+                <Page1 />
+                <Page2 />
+            </div>
+        </>
+    );
+}
+
+const useStyles = tss.withName({ Gmeta }).create(({theme}) => {
+    return {
+        root: {
+            width: "100%",
+            height: "100%",
+            display: "flex",
+            flexDirection: "column",
+            gap: theme.spacing(6)
+        }
+    };
+});
